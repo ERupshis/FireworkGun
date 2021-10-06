9073f9472110ce40fe8f513fca5a0913f2d759e3
Эдгар Рупшис <e.rupshis@gmail.com>
2021-10-06 17:30:42 +0200

    Need to refactory Printer

--- FireworkGun.cpp
+++ FireworkGun.cpp
@@ -1,261 +1,110 @@
 // FireworkGun.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
-#include <vector>
-#include <memory>
-#include <Windows.h>
-#include <cstdlib>
-#include <tuple>
-#include <list>
-#include <optional>
-#include <cmath>
-#include <set>
+#include <sstream>
 
-struct Coord {
-    int x = 0;
-    int y = 0;
+#include "shell.h"
 
-    bool operator < (Coord other) const {
-        if (x < other.x) {
-            return true;
-        }
-        else if (x == other.x) {
-            return y < other.y;
-        }        
-        return false;
-    }
-};
+using namespace std::literals;
 
-struct ShellData {
-    ShellData() = default;
-
-    ShellData(int time, int life_time, int velocity, int direction_angle, Coord coord)
-        : begin_time_(time), 
-        life_time_(life_time), 
-        velocity_(velocity), 
-        direction_angle_(direction_angle), 
-        coord_(coord)
-    {
-    }
-
-    int begin_time_ = 0;
-    int life_time_ = 0;
-    double velocity_ = 0;
-    double velocity_curr_ = 0;
-    int direction_angle_ = 0;
-    Coord coord_ = { 0,0 };
-    Coord coord_curr_ = { 0,0 };
-};
-
-enum class Child {
-    FRAGMENT,
-    SPARK
-};
-
-class Shell {
+class Printer {
 public:
-    Shell(int time) // constructor for CannonShell       
-    {   
-        data_.begin_time_ = time;
-        data_.coord_ = { 0, 0 };
-        data_.coord_curr_= { 0, 0 };
-        data_.direction_angle_ = 30 + (std::rand() % 120);//
-        data_.life_time_ = std::rand() % 15 + 8;
-        data_.velocity_ = 20.0 + std::rand() % 20;       
-    }
-
-    Shell(int time, const ShellData& parent, enum class Child type) // Constructor for FragmentShell or Spark
-    {
-        data_.begin_time_ = time;
-        data_.coord_ = parent.coord_curr_;
-        data_.coord_curr_ = parent.coord_curr_;
-        data_.life_time_ = parent.life_time_ / 2;
-        if (type == Child::FRAGMENT) {
-            int rand_angle = (std::rand() % 40) - 20; // 6 degree deviation
-            data_.direction_angle_ = parent.direction_angle_ + rand_angle;
-            data_.velocity_ = parent.velocity_curr_ * 2;
+    Printer(int x, int y, int shells_count) 
+        : frame_({ x, y }) {
+        for (int i = 0; i < shells_count; ++i) {
+            objects.push_back(std::make_unique<CannonShell>(CannonShell(0)));
         }
-        else {
-            data_.direction_angle_ = (std::rand() % 359);            
-            data_.velocity_ = parent.velocity_curr_ + 2 * parent.life_time_;
-        }
-    }    
-    
-    std::vector<std::unique_ptr<Shell>> Refresh(int time) {
-        if (data_.life_time_ + data_.begin_time_ <= time) {
-            return Destroy(time);            
-        }
-        UpdShell(time);
-        return {};
-    }   
-    
-    virtual ~Shell() {}
-
-    ShellData GetData() const {
-        return data_;
     }
 
-    void SetData(const ShellData& data) {
-        data_ = data;
-    }
-private:
-    ShellData data_;  
-
-    virtual std::vector<std::unique_ptr<Shell>> Destroy(int time) = 0;
-    
-
-    void UpdShell(int time) {
-        ShellData data = static_cast<Shell*>(this)->GetData();
-        static_cast<Shell*>(this)->SetData(UpdShellCurrCoords(data, time));
-    }
-
-    ShellData UpdShellCurrCoords(const ShellData& data, int time) {
-        ShellData res = data;
-
-        int x = res.velocity_ * std::cos(res.direction_angle_ * std::acos(-1) / 180) * (time - res.begin_time_) / 10;
-        int y = res.velocity_ * std::sin(res.direction_angle_ * std::acos(-1) / 180) * (time - res.begin_time_) / 10 - 4.9 * (time - res.begin_time_) / 15 * (time - res.begin_time_) /10;
-
-        res.velocity_curr_ = std::sqrt((res.velocity_ * std::cos(res.direction_angle_ * std::acos(-1) / 180))
-            * (res.velocity_ * std::cos(res.direction_angle_ * std::acos(-1) / 180))
-            + (res.velocity_ * std::sin(res.direction_angle_ * std::acos(-1) / 180) - 9,8 * (time - res.begin_time_) / 10
-            * (res.velocity_ * std::sin(res.direction_angle_ * std::acos(-1) / 180) - 9,8 * (time - res.begin_time_) / 10)));
-
-        res.coord_curr_.x = res.coord_.x + x;
-        res.coord_curr_.y = ((res.coord_.y + y < 0) ? 0 : res.coord_.y + y);
-        return res;
-    }
-};
-
-class Spark : public Shell {
-public:
-    Spark(int time, const ShellData& parent_data)
-        :Shell(time, parent_data, Child::SPARK) {
-    }
-private:
-    std::vector<std::unique_ptr<Shell>> Destroy(int time) override {
-        ShellData& data = static_cast<Shell*>(this)->GetData();
-        data.begin_time_ = -1;
-        static_cast<Shell*>(this)->SetData(data);
-        return {};
-    }
-       
-};
-
-class ShellFragment : public Shell {
-public:
-    ShellFragment(int time, const ShellData& parent_data)
-        :Shell(time, parent_data, Child::FRAGMENT) {
-    }
-    
-private:
-    std::vector<std::unique_ptr<Shell>> Destroy(int time) override {
-        std::vector<std::unique_ptr<Shell>> res;
-        ShellData& data = static_cast<Shell*>(this)->GetData();
-
-        size_t sparks_num = (std::rand() % 3); // tenth
-        for (size_t i = 0; i < ((sparks_num == 0) ? 10 : sparks_num * 10); ++i) {
-            res.push_back(std::make_unique<Spark>(Spark(time, data))); // Spark!!!
-        }
-        data.begin_time_ = -1;
-        static_cast<Shell*>(this)->SetData(data);
-        return res;
-    }    
-};
-
-class CannonShell : public Shell {
-public:
-    CannonShell(int time)
-       :Shell(time) {       
-    }       
-
-    ~CannonShell() {        
-    }
-private:
-    std::vector<std::unique_ptr<Shell>> Destroy(int time) override {
-        std::vector<std::unique_ptr<Shell>> res;
-        ShellData data = static_cast<Shell*>(this)->GetData();
-
-        if (std::rand() % 2) {
-            res.push_back(std::make_unique<ShellFragment>(ShellFragment(time, data)));
-            res.push_back(std::make_unique<ShellFragment>(ShellFragment(time, data)));
-        }
-        else {
-            size_t sparks_num = (std::rand() % 4); // tenth
-            for (size_t i = 0; i < ((sparks_num == 0) ? 10 : sparks_num * 10); ++i) {
-                res.push_back(std::make_unique<Spark>(Spark(time, data))); // Spark!!!
-            }
-        }
-        data.begin_time_ = -1;
-        static_cast<Shell*>(this)->SetData(data);
-        return res;
-    }
-
-    
-    
-};
-
-int main()
-{   
-    std::srand(std::time(NULL));
-    int time = 0;
-    std::list<std::unique_ptr<Shell>> objects;    
-    for (int i = 0; i < 20; ++i) {
-        objects.push_back(std::make_unique<CannonShell>(CannonShell(time)));
-    }
-    while (objects.size() > 0) {
-        //int min_x = INT32_MAX, max_x = INT32_MIN, min_y = INT32_MAX, max_y = INT32_MIN;
-        int min_x = -100, max_x = 100, min_y = 0, max_y = 75;
-        std::set<Coord> objects_coords;        
-        Sleep(600);        
+    std::stringstream Draw(int time) {
+        std::stringstream out;
+        std::set<Coord> objects_coords;
         std::vector<std::list<std::unique_ptr<Shell>>::iterator> obj_to_remove;
+
         for (auto& it = objects.begin(); it != objects.end(); ++it) {
             std::vector<std::unique_ptr<Shell>> tmp = it->get()->Refresh(time);
             if (tmp.size() != 0) { // -1 - mark of spark                
                 for (size_t i = 0; i < tmp.size(); ++i) {
                     objects.push_back(std::move(tmp[i]));
                 }
-                
-            } 
-            const ShellData& shell_data = it->get()->GetData();
-            if (shell_data.begin_time_ == -1) {
+
+            }
+            ShellData shell_b_data = it->get()->GetData('b');
+            ShellData shell_c_data = it->get()->GetData('c');
+            if (shell_c_data.begin_time_ == -1) {
                 obj_to_remove.push_back(it);
             }
-            else {                
-                objects_coords.insert(shell_data.coord_curr_);
+            else {
+                objects_coords.insert(shell_c_data.coord_);
             }
         }
-
 
         for (int i = obj_to_remove.size() - 1; i >= 0; --i) {
             objects.erase(obj_to_remove[i]);
         }
-        for (int y = max_y; y >= min_y - 3; --y) {
-            for (int x = min_x; x <= max_x; ++x) {
-                if (y >= min_y) {
-                    if (objects_coords.count({x, y })) {
-                        std::cout << 'X';
+
+        for (int y = frame_.y; y >= - 3; --y) {
+            for (int x = -frame_.x / 2; x <= frame_.x / 2; ++x) {
+                if (y >= 0) {
+                    if (objects_coords.count({ x, y })) {
+                        out << 'X';
                     }
                     else {
-                        std::cout << '-';
+                        out << ' ';
                     }
                 }
                 else {
                     if (-1 <= x && x <= 1) {
-                        std::cout << 'G';
+                        out << 'G';
                     }
+                    else if (y != -3) {
+                        if (objects_coords.count({ x, y })) {
+                            out << 'X';
+                        }
+                        else {
+                            out << ' ';
+                        }
+                    }                    
                     else {
-                        std::cout << ' ';
+                        out << '_';
                     }
                 }
 
             }
-            std::cout << '\n';
+            out << '\n';
         }
-                  
-        std::cout << '\n' << '\n' << '\n';
-        ++time;
+        out << "Live obejcts: "s << objects.size();
+        out << '\n';
+        return out;
+    }
 
+
+
+    int GetObjectsCount() const {
+        return objects.size();
+    }
+
+private:
+    struct frame {
+        int x = 0;
+        int y = 0;
+    } frame_;
+    std::list<std::unique_ptr<Shell>> objects;
+};
+
+int main()
+{   
+    int n;
+    std::cout << "Enter number of cannon shels: "s;
+    std::cin >> n;
+
+    std::srand(std::time(NULL));
+    int time = 0;
+    Printer printer(200, 75, n);
+    while (printer.GetObjectsCount() > 0) {
+        Sleep(500);
+        std::cout << printer.Draw(time).str();
+        ++time;
     }
 }
 
--- shell.h
+++ shell.h
@@ -1,2 +1,205 @@
 #pragma once
 
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <Windows.h>
+#include <cstdlib>
+#include <tuple>
+#include <list>
+#include <optional>
+#include <cmath>
+#include <set>
+
+struct Coord {
+    int x = 0;
+    int y = 0;
+
+    bool operator < (Coord other) const {
+        if (x < other.x) {
+            return true;
+        }
+        else if (x == other.x) {
+            return y < other.y;
+        }
+        return false;
+    }
+};
+
+struct ShellData {
+    ShellData() = default;
+
+    ShellData(int time, int life_time, int velocity, int direction_angle, Coord coord)
+        : begin_time_(time), life_time_(life_time), velocity_(velocity),
+        direction_angle_(direction_angle), coord_(coord)
+    {
+    }
+
+    int begin_time_ = 0;
+    int life_time_ = 0;
+    double velocity_ = 0;
+    int direction_angle_ = 0;
+    Coord coord_ = { 0,0 };
+};
+
+enum class Child {
+    FRAGMENT,
+    SPARK
+};
+
+namespace shell_sup { // evaluation methods
+    double GetRadianAngle(int degree) {
+        return degree * std::acos(-1) / 180.0;
+    }
+
+    int CountCoordX(int time, int divider, const ShellData& b_data) {
+        return b_data.velocity_ * std::cos(GetRadianAngle(b_data.direction_angle_)) * (time - b_data.begin_time_) / divider;
+    }
+
+    int CountCoordY(int time, int divider, const ShellData& b_data) {
+        return (b_data.velocity_ * std::sin(GetRadianAngle(b_data.direction_angle_)) * (time - b_data.begin_time_)
+            - 4.9 * (time - b_data.begin_time_) / divider * (time - b_data.begin_time_)) / divider;
+    }
+
+    double CountCurrSpeed(int time, int divider, const ShellData& b_data) { // sqrt((Vo*cosa)^2 + (Vo*sina - g*t))
+        double g = 9.8;
+        int elaps_time = (time - b_data.begin_time_) / divider;
+        return std::sqrt(std::pow(b_data.velocity_ * std::cos(shell_sup::GetRadianAngle(b_data.direction_angle_)), 2.0)
+            + (b_data.velocity_ * std::sin(shell_sup::GetRadianAngle(b_data.direction_angle_)) - g * elaps_time, 2.0));
+    }
+}
+/*-------------------------------------------------------------*/
+class Shell {    
+public:
+    Shell(int time) // constructor for CannonShell       
+    {
+        b_data_.begin_time_ = time;
+        b_data_.coord_ = { 0, 0 };
+        b_data_.direction_angle_ = 30 + (std::rand() % 120);
+        b_data_.life_time_ = 8 +(std::rand() % 15);
+        b_data_.velocity_ = 20.0 + (std::rand() % 20);
+
+        c_data_.life_time_ = b_data_.life_time_;
+    }
+
+    Shell(int time, const ShellData& c_parent, enum class Child type) // Constructor for FragmentShell or Spark
+    {
+        b_data_.begin_time_ = time;
+        b_data_.coord_ = c_parent.coord_;
+        b_data_.life_time_ = c_parent.life_time_ / 2;
+        if (type == Child::FRAGMENT) {
+            int rand_angle = (std::rand() % 40) - 20; // +-20 degree deviation
+            b_data_.direction_angle_ = c_parent.direction_angle_ + rand_angle;
+            b_data_.velocity_ = c_parent.velocity_ * 2;
+        }
+        else {
+            b_data_.direction_angle_ = (std::rand() % 359); // random angle of sprak direction
+            b_data_.velocity_ = c_parent.velocity_ + 2 * c_parent.life_time_;
+        }
+    }
+
+    std::vector<std::unique_ptr<Shell>> Refresh(int time) { // if method returns nothing - object dead time didn't come or it's a final stage of object life,                                                             )
+        if (b_data_.life_time_ + b_data_.begin_time_ <= time) {
+            return Destroy(time); // pure virtual. Each type of object has its own definition
+                                  // if result is {} - endlife of object without new child objects (sparks of fragments),
+                                  //otherwise it was split on smaller object (fragmens of spraks)
+        }
+        UpdShellCurrData(time); // refresh current coords and speed of shell
+        return {}; // object is in the mid of its life
+    }
+
+    virtual ~Shell() {}
+
+    ShellData GetData(char type) const {
+        if (type == 'b') {
+            return b_data_;
+        }
+        else {
+            return c_data_;
+        }        
+    }
+
+    void SetData(const ShellData& data) { // apliccable only for current data
+        c_data_ = data;
+    }
+private:
+    ShellData b_data_ = {}; // data in the beginning
+    ShellData c_data_ = {}; // data at the moment of time
+
+    virtual std::vector<std::unique_ptr<Shell>> Destroy(int time) = 0; // responsible for life of object and creation new ones       
+
+    void UpdShellCurrData(int time) { // responsible for current speed and position calculation        
+        int divider = 10; // reduce distance of object move on picture
+
+        int d_x = shell_sup::CountCoordX(time, divider, b_data_);
+        int d_y = shell_sup::CountCoordY(time, divider, b_data_);
+
+        c_data_.coord_.y = ((b_data_.coord_.y + d_y < -2) ? -2 : b_data_.coord_.y + d_y); // objects cannot fall bellow floor
+        c_data_.coord_.x = ((c_data_.coord_.y == -2) ? c_data_.coord_.x : b_data_.coord_.x + d_x);        
+        c_data_.velocity_ = shell_sup::CountCurrSpeed(time, divider, b_data_);        
+    }
+};
+/*-------------------------------------------------------------*/
+class Spark : public Shell {
+public:
+    Spark(int time, const ShellData& parent_data)
+        :Shell(time, parent_data, Child::SPARK) {
+    }
+private:
+    std::vector<std::unique_ptr<Shell>> Destroy(int time) override {
+        ShellData c_data = GetData('c');        
+        c_data.begin_time_ = -1; // marker for dead object
+        SetData(c_data);
+        return {}; // no new object at after death
+    }
+};
+/*-------------------------------------------------------------*/
+class ShellFragment : public Shell {
+public:
+    ShellFragment(int time, const ShellData& parent_data)
+        :Shell(time, parent_data, Child::FRAGMENT) {
+    }
+
+private:
+    std::vector<std::unique_ptr<Shell>> Destroy(int time) override {
+        std::vector<std::unique_ptr<Shell>> res;
+        ShellData c_data = GetData('c'); // use current parent data for new objects
+
+        size_t sparks_num = (std::rand() % 3); // tenth
+        for (size_t i = 0; i < (10 + sparks_num * 10); ++i) {
+            res.push_back(std::make_unique<Spark>(Spark(time, c_data))); // new sparks only
+        }
+        
+        c_data.begin_time_ = -1; // marker of dead object
+        SetData(c_data);
+        return res; // new objects
+    }
+};
+
+class CannonShell : public Shell {
+public:
+    CannonShell(int time)
+        :Shell(time) {
+    }
+
+private:
+    std::vector<std::unique_ptr<Shell>> Destroy(int time) override {
+        std::vector<std::unique_ptr<Shell>> res;
+        ShellData c_data = GetData('c');
+
+        if (std::rand() % 2) { // cannon shell splits on 2 fragments
+            res.push_back(std::make_unique<ShellFragment>(ShellFragment(time, c_data)));
+            res.push_back(std::make_unique<ShellFragment>(ShellFragment(time, c_data)));
+        }
+        else { // cannon shell splits on 10x sparks
+            size_t sparks_num = (std::rand() % 3); // tenth
+            for (size_t i = 0; i < (10 + sparks_num * 10); ++i) {
+                res.push_back(std::make_unique<Spark>(Spark(time, c_data))); // Spark!!!
+            }
+        }
+       
+        c_data.begin_time_ = -1; // marker of dead object
+        SetData(c_data);
+        return res;
+    }
+};
